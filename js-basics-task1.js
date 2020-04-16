396e4d481af7882e170b2357fe11382ff3eb39f9
https://github.com/mishhubc <mihaijicum@gmail.com>
2020-04-16 21:07:19 +0200

    task 1 it academy

---
+++ js-basics-task1.js
@@ -0,0 +1,10 @@
+const yourName = 'Mihai Jicu';
+const birthDate = new Date('1989-10-15');
+let num1 = 3;
+num2 = num1;
+num3 = num2;
+
+
+console.log(`Hello! This program has been written by ${yourName}`);
+console.log(`Author has been born on ${birthDate}`);
+console.log(`The result of hidden equation is: ${(num1 + num2) * num3}`);
